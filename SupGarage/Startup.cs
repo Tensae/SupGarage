85d0572878cd97deec3f0e7fbc69da2efd9faf4d
User <User@DESK-CLZ7J32>
2017-10-23 09:36:14 +0200

    Add project files.

---
+++ SupGarage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SupGarage.Startup))]
+namespace SupGarage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
